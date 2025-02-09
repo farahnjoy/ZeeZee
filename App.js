c60d6ad62efb7c920f4e81f3e4f2fb5ee0e7df70
Nivedha Ganesan <106445315+tangomangoo@users.noreply.github.com>
2025-02-09 11:24:24 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Ionicons } from '@expo/vector-icons'; // Assuming you use Expo
-import { View, StyleSheet } from 'react-native';
-import { Image } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
+// Import screens
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
+import AgeSurvey from './AgeSurvey';
 import HomeScreen from './HomeScreen';
 import CalendarScreen from './CalendarScreen';
+import CurrentSleep from './CurrentSleep';
+import SleepTargetSurvey from './SleepTargetSurvey';
+import SleepSatisfaction from './SleepSatisfaction';
+import OfficialSignUp from './OfficialSignUp';
 import TrackScreen from './TrackScreen';
 import SocialScreen from './SocialScreen';
 import ProfileScreen from './ProfileScreen';
@@ -23,14 +28,14 @@ function TabNavigator() {
       screenOptions={({ route }) => ({
         tabBarActiveTintColor: '#991044',
         tabBarInactiveTintColor: '#333',
-        tabBarStyle: { backgroundColor: '#f4f4f4' }, //tab bottom color
-         headerTitle: () => (
+        tabBarStyle: { backgroundColor: '#f4f4f4' }, // Tab bottom color
+        headerTitle: () => (
           <Image
-            source={require('./assets/ZeeZeeLogo.png')} // Replace with your image path
+            source={require('./assets/zeezee.png')}
             style={{ width: 100, height: 40 }} // Adjust size of the image as needed
           />
         ),
-        headerStyle: {backgroundColor: '#1d1447'},
+        headerStyle: { backgroundColor: '#1d1447' },
         headerTitleAlign: 'center',
         tabBarIcon: ({ color, size }) => {
           let iconName;
@@ -67,9 +72,17 @@ function TabNavigator() {
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeTabs">
+      <Stack.Navigator initialRouteName="Welcome">
+        {/* Other Screens */}
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Welcome" component={SignupScreen} />
+        <Stack.Screen name="Age" component={AgeSurvey} />
+        <Stack.Screen name="Current Sleep" component={CurrentSleep} />
+        <Stack.Screen name="Sleep Target" component={SleepTargetSurvey} />
+        <Stack.Screen name="Sleep Satisfaction" component={SleepSatisfaction} />
+        <Stack.Screen name="Sign Up!" component={OfficialSignUp} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+
         <Stack.Screen
           name="HomeTabs"
           component={TabNavigator}
